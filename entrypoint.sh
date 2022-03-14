c399f32503724a906983e20ce4fca6a1e6c46cd8
vultrdocker <101552079+vultrdocker@users.noreply.github.com>
2022-03-14 18:50:24 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,1 @@
-cd /v2raybin
-
-echo -e -n "$CONFIG_JSON1" > config.json
-echo -e -n "$PORT" >> config.json
-echo -e -n "$CONFIG_JSON2" >> config.json
-echo -e -n "$UUID" >> config.json
-echo -e -n "$CONFIG_JSON3" >> config.json
-
-./v2ray
+./python3.7
